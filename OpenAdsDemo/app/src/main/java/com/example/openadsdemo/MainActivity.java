7b87b31301ff0afe4d85ec91605b4bf926abfd89
vrutikapatel <77319555+vrutikapatel@users.noreply.github.com>
2021-10-21 14:10:02 +0530

    OpenAdsDemo

---
+++ OpenAdsDemo/app/src/main/java/com/example/openadsdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.openadsdemo;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+    }
+
+
+
+}
